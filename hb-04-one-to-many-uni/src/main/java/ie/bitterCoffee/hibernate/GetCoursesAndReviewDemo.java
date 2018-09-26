930f4d86102959516ec4100decb1ee706726349c
Michael-IRL <iszekeell@gmail.com>
2018-09-26 22:21:58 +0100

    Add demo app for getting coures and reviews

---
+++ hb-04-one-to-many-uni/src/main/java/ie/bitterCoffee/hibernate/GetCoursesAndReviewDemo.java
@@ -0,0 +1,57 @@
+package ie.bitterCoffee.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ie.bitterCoffee.hibernate.entity.Course;
+import ie.bitterCoffee.hibernate.entity.Instructor;
+import ie.bitterCoffee.hibernate.entity.InstructorDetail;
+import ie.bitterCoffee.hibernate.entity.Review;
+
+public class GetCoursesAndReviewDemo
+{
+
+	public static void main(String[] args)
+	{
+		//Create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.addAnnotatedClass(Review.class)
+									.buildSessionFactory();
+		
+		//Create session
+		Session session = factory.getCurrentSession();
+		
+		try
+		{						
+			//start a transaction
+			session.beginTransaction();
+			
+			int theId = 1;
+			
+			Course course = session.get(Course.class, theId);
+			
+			System.out.println("The course: "+course);
+			System.out.println("The course reviews: "+course.getReviews());
+			
+			//commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+			factory.close();
+		}			
+	}
+
+}
